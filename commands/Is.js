73b073b6519b7a7f8caa98637980578113f5f620
aryan <aryankuthakur@hotmail.com>
2020-11-04 15:09:56 -0500

    add is

---
+++ commands/Is.js
@@ -0,0 +1,20 @@
+/*jshint esversion: 8 */
+
+const Discord = require('discord.js');
+var fs = require('fs');
+var path = require('path');
+
+module.exports = {
+    run: async(message, args, client) => {
+    var data = require("../homeworkData.json");
+
+    const send = new Discord.MessageEmbed().setTitle('Current Homework');
+
+    for (var i in data) {
+        console.log(i);
+        send.addField(i,"Due date: " + data[i].date + " " + " Description: " + data[i].desc);
+    }
+
+    message.channel.send(send);
+    }
+};
