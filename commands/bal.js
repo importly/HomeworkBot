3068c003ca04d5c369acb73d2668eb660cc3560b
importly <aryankuthakur@hotmail.com>
2021-01-21 10:32:55 -0500

    sd

---
+++ commands/bal.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 8 */
+
+const Discord = require("discord.js");
+let fs = require("fs");
+let path = require("path");
+
+module.exports = {
+  run: async (message, args, client) => {
+    
+  },
+};
